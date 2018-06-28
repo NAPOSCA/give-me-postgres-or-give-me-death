5d52da68aee8555adcd55a3fbff41954feefba71
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-28 18:21:46 -0400

    Initial commit

---
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/Pantryplus2electricboogalooApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.pantryplus2electricboogaloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pantryplus2electricboogalooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pantryplus2electricboogalooApplication.class, args);
+	}
+}
---
+++ src/test/java/org/wecancodeit/pantryplus2electricboogaloo/Pantryplus2electricboogalooApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.pantryplus2electricboogaloo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pantryplus2electricboogalooApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
