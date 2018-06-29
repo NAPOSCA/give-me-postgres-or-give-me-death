d4d634a5ad72aca03767406ca7f79bf6883210c2
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-29 16:57:18 -0400

    Added @RestController annotation

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
@@ -1,5 +1,8 @@
 package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CartRestController {
 
 }
