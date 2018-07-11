d2da8ad71b1cc6b7ac588143ed8d7aa2576726f4
theoccasionalist <mrmccall07@gmail.com>
2018-07-11 14:43:45 -0400

    addded attach category to model test

--- src/test/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/AdminMockMvcTest.java
+++ src/test/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/AdminMockMvcTest.java
@@ -1,6 +1,9 @@
 package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import org.wecancodeit.pantryplus2electricboogaloo.category.Category;
 import org.wecancodeit.pantryplus2electricboogaloo.category.CategoryRepository;
 
 public class AdminMockMvcTest {
@@ -21,15 +25,28 @@ public class AdminMockMvcTest {
 	@Mock
 	private CategoryRepository categoryRepo;
 	
+	@Mock
+	private Category category;
+	
+	@Mock
+	private Category category2;
+	
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}	
 	
 	@Test
-	public void shouldReturnAdminCategoryView() {
-		String templateName = "admin/categories";
+	public void shouldReturnAdminCategoriesView() {
 		String actualName =  adminController.displayAdminCategoriesView(model);
-		assertEquals(actualName, templateName);
+		assertEquals(actualName, "admin/categories");
+	}
+	
+	@Test
+	public void shouldAttachCategoryToModel() {
+		Iterable<Category> categories = asList(category, category2);
+		when(categoryRepo.findAll()).thenReturn(categories);
+		adminController.displayAdminCategoriesView(model);
+		verify(model).addAttribute("categories", categories);
 	}
 }
