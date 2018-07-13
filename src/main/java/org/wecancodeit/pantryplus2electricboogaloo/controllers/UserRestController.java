40efd3628097439f0f0775a1fbeb4947caabc0fd
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-07-13 12:41:17 -0400

    Created method to patch firstName

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserRestController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserRestController.java
@@ -1,5 +1,13 @@
 package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
-public class UserRestController {
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.wecancodeit.pantryplus2electricboogaloo.user.PantryUser;
+
+public class UserRestController extends LoginController {
+
+	public void updateUsersFirstName(OAuth2AuthenticationToken token, String firstName) {
+		PantryUser user = resolveUser(token);
+		user.updateFirstName(firstName);
+	}
 
 }
--- src/test/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserRestControllerMockTest.java
+++ src/test/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserRestControllerMockTest.java
@@ -1,6 +1,59 @@
 package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.wecancodeit.pantryplus2electricboogaloo.user.PantryUser;
+import org.wecancodeit.pantryplus2electricboogaloo.user.UserRepository;
+
 public class UserRestControllerMockTest {
 
+	@InjectMocks
 	private UserRestController underTest;
+
+	private String firstName;
+
+	@Mock
+	private OAuth2AuthenticationToken token;
+
+	@Mock
+	private PantryUser user;
+
+	@Mock
+	private OAuth2User oAuth2User;
+
+	@Mock
+	private Map<String, Object> attributes;
+
+	private String googleName;
+	
+	@Mock
+	private UserRepository userRepo;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		when(token.getPrincipal()).thenReturn(oAuth2User);
+		when(oAuth2User.getAttributes()).thenReturn(attributes);
+		googleName = "12345";
+		when(attributes.get("sub")).thenReturn(googleName);
+		when(userRepo.findByGoogleName(googleName)).thenReturn(Optional.of(user));
+		firstName = "Scooby";
+	}
+
+	@Test
+	public void shouldHaveUpdateUsersFirstNameUpdateFirstName() {
+		underTest.updateUsersFirstName(token, firstName);
+		verify(user).updateFirstName(firstName);
+	}
 }
