49517468a440327e3c46bbd170b4b5d2c0b37913
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-07-16 17:42:12 -0400

    css, images, and js files now shouldn't require authentication to view

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/SecurityConfig.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/SecurityConfig.java
@@ -11,7 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http //
 				.authorizeRequests() //
-				.antMatchers("/").permitAll() //
+				.antMatchers("/", "/css/**", "/images/**", "/js/**").permitAll() //
 				.anyRequest().authenticated();
 	}
 
