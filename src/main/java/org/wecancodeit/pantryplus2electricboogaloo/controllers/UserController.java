07f4c67cf7a6c0b9742ce4e1097d8d5a0e0d1c72
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-07-19 00:30:05 -0400

    Tested out more of the information that we want from the form

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserController.java
@@ -1,24 +1,40 @@
 package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
+import javax.annotation.Resource;
+
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.wecancodeit.pantryplus2electricboogaloo.user.PantryUser;
+import org.wecancodeit.pantryplus2electricboogaloo.user.UserRepository;
 
 @Controller
 public class UserController extends LoginController {
 
-	@PostMapping("/user")
-	public String receivePostOnUser(OAuth2AuthenticationToken token, @RequestParam String firstName,
+	@Resource
+	private UserRepository userRepo;
+
+	@RequestMapping("/user")
+	public String receiveRequestOnUser(OAuth2AuthenticationToken token, @RequestParam String firstName,
 			@RequestParam String lastName, @RequestParam String address, @RequestParam int familySize,
-			@RequestParam String birthdate) {
+			@RequestParam String birthdate, @RequestParam int schoolAgeChildren, @RequestParam String zipCode,
+			@RequestParam String referral, @RequestParam(required = false) boolean hasInfants) {
+		System.out.println(firstName);
+		System.out.println(hasInfants);
 		PantryUser user = resolveUser(token);
 		user.updateFirstName(firstName);
 		user.updateLastName(lastName);
 		user.updateAddress(address);
 		user.updateFamilySize(familySize);
 		user.updateBirthdate(birthdate);
+		if (familySize >= schoolAgeChildren) {
+			user.updateSchoolAgeChildren(schoolAgeChildren);
+		}
+		user.updateZipCode(zipCode);
+		user.updateReferral(referral);
+		user.updateHasInfants(hasInfants);
+		userRepo.save(user);
 		return "redirect:/settings";
 	}
 }
--- src/test/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserControllerMockTest.java
+++ src/test/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserControllerMockTest.java
@@ -2,6 +2,7 @@ package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -19,13 +20,13 @@ import org.wecancodeit.pantryplus2electricboogaloo.user.PantryUser;
 import org.wecancodeit.pantryplus2electricboogaloo.user.UserRepository;
 
 public class UserControllerMockTest {
-	
+
 	@InjectMocks
 	private UserController underTest;
-	
+
 	@Mock
 	private OAuth2AuthenticationToken token;
-	
+
 	@Mock
 	private OAuth2User oAuth2User;
 
@@ -51,7 +52,13 @@ public class UserControllerMockTest {
 	private String birthdate;
 
 	private int schoolAgeChildren;
-	
+
+	private String zipCode;
+
+	private String referral;
+
+	private boolean hasInfants;
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
@@ -66,41 +73,94 @@ public class UserControllerMockTest {
 		familySize = 5;
 		birthdate = "September 13, 1969";
 		schoolAgeChildren = 0;
+		zipCode = "54321";
+		referral = "COMPASS";
+		hasInfants = false;
 	}
 
 	@Test
 	public void shouldUpdateFirstNameWhenReceivingPostOnUser() {
-		underTest.receivePostOnUser(token, firstName, lastName, address, familySize, birthdate);
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
 		verify(user).updateFirstName(firstName);
 	}
-	
+
 	@Test
 	public void shouldUpdateLastNameWhenReceivingPostOnUser() {
-		underTest.receivePostOnUser(token, firstName, lastName, address, familySize, birthdate);
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
 		verify(user).updateLastName(lastName);
 	}
-	
+
 	@Test
 	public void shouldUpdateAddressWhenReceivingPostOnUser() {
-		underTest.receivePostOnUser(token, firstName, lastName, address, familySize, birthdate);
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
 		verify(user).updateAddress(address);
 	}
-	
+
 	@Test
 	public void shouldUpdateFamilySizeWhenReceivingPostOnUser() {
-		underTest.receivePostOnUser(token, firstName, lastName, address, familySize, birthdate);
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
 		verify(user).updateFamilySize(familySize);
 	}
-	
+
 	@Test
 	public void shouldUpdateBirthDateWhenReceivingPostOnUser() {
-		underTest.receivePostOnUser(token, firstName, lastName, address, familySize, birthdate);
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
 		verify(user).updateBirthdate(birthdate);
 	}
-	
+
 	@Test
 	public void shouldRedirectBackToFormWhenReceivingPostOnUser() {
-		String actual = underTest.receivePostOnUser(token, firstName, lastName, address, familySize, birthdate);
+		String actual = underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate,
+				schoolAgeChildren, zipCode, referral, hasInfants);
 		assertThat(actual, is("redirect:/settings"));
+	}
+
+	@Test
+	public void shouldUpateSchoolAgeChildrenWhenReceivingRequestOnUser() {
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
+		verify(user).updateSchoolAgeChildren(schoolAgeChildren);
+	}
+
+	@Test
+	public void shouldSaveUserAfterUpdatingWhenReceivingRequestOnUser() {
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
+		verify(userRepo).save(user);
+	}
+
+	@Test
+	public void shouldUpdateZipCodeWhenReceivingRequestOnUser() {
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
+		verify(user).updateZipCode(zipCode);
+	}
+
+	@Test
+	public void shouldUpdateReferralWhenReceivingRequestOnUser() {
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
+		verify(user).updateReferral(referral);
+	}
+
+	@Test
+	public void shouldUpdateHasInfantsWhenReceivingRequestOnUser() {
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
+		verify(user).updateHasInfants(hasInfants);
+	}
+
+	@Test
+	public void shouldNotUpdateSchoolAgeChildrenIfThereAreMoreThanTotalFamilySize() {
+		familySize = 4;
+		schoolAgeChildren = 5;
+		underTest.receiveRequestOnUser(token, firstName, lastName, address, familySize, birthdate, schoolAgeChildren,
+				zipCode, referral, hasInfants);
+		verify(user, never()).updateSchoolAgeChildren(schoolAgeChildren);
 	}
 }
