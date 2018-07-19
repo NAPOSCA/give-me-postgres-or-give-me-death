5e6bba92c92bf637055c210b96d9d410b8b17ccc
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-07-18 20:20:26 -0400

    Added mapping

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/UserController.java
@@ -2,12 +2,14 @@ package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.wecancodeit.pantryplus2electricboogaloo.user.PantryUser;
 
 @Controller
 public class UserController extends LoginController {
 
+	@PostMapping("/user")
 	public String receivePostOnUser(OAuth2AuthenticationToken token, @RequestParam String firstName,
 			@RequestParam String lastName, @RequestParam String address, @RequestParam int familySize,
 			@RequestParam String birthdate) {
