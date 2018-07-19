68aa9e814fb7717540cfac8ec3e89cc186571f25
theoccasionalist <mrmccall07@gmail.com>
2018-07-19 14:27:41 -0400

    too legit to commit

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/AdministrationController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/AdministrationController.java
@@ -1,15 +1,19 @@
 package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.util.Optional;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.wecancodeit.pantryplus2electricboogaloo.category.Category;
 import org.wecancodeit.pantryplus2electricboogaloo.category.CategoryRepository;
 import org.wecancodeit.pantryplus2electricboogaloo.product.LimitedProduct;
 import org.wecancodeit.pantryplus2electricboogaloo.product.PricedProduct;
@@ -49,5 +53,34 @@ public class AdministrationController {
 			return "admin/limited-product";
 		}
 		return "admin/product";
+	}
+	
+	@RequestMapping(value = "/admin/categories", method = POST)
+	public String receiveAPostRequestOnCategories(@RequestParam String categoryName) {
+		categoryRepo.save(new Category(categoryName));
+		return "redirect:/admin/categories";
+	}
+
+	@Transactional
+	@RequestMapping(value = "/admin/categories/{categoryId}/products", method = POST)
+	public String receiveAPostRequestOnACategorysProducts(Model model, @PathVariable Long categoryId,
+			@RequestParam String type, @RequestParam String productName, @RequestParam(required = false) String image,
+			@RequestParam(defaultValue = "0") int maximumQuantity, @RequestParam(defaultValue = "0") int price) {
+		Category category = categoryRepo.findById(categoryId).get();
+		if (type.equals("Product")) {
+			Product product = new Product(productName, category, image);
+			productRepo.save(product);
+		} else if (type.equals("LimitedProduct")) {
+			LimitedProduct product = new LimitedProduct(productName, category, image, maximumQuantity);
+			productRepo.save(product);
+		} else if (type.equals("PricedProduct")) {
+			PricedProduct product = new PricedProduct(productName, category, image, maximumQuantity, price);
+			productRepo.save(product);
+		}
+		entityManager.flush();
+		entityManager.clear();
+		category = categoryRepo.findOne(categoryId);
+		model.addAttribute("category", category);
+		return "redirect:/admin/categories/" + categoryId;
 	}
 }
