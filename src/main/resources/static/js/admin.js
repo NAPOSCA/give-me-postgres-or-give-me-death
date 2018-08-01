3c716e2961bd1aa7874193fcd95d6f4ba939391b
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-08-01 01:07:39 -0400

    Added csrf token to ajax requests

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -10,14 +10,19 @@ function initialize() {
 	});
 }
 
-function request(callback, method, path) {
+const request = (callback, method, url) => {
 	const xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = () => {
-		if (xhr.readyState == 4 && xhr.status == 200) {
+		if (xhr.readyState === 4 && xhr.status === 200) {
 			const response = JSON.parse(xhr.response);
 			callback(response);
 		}
 	};
-	xhr.open(method, path, true);
+	xhr.open(method, url, true);
+	const token = getMetaContent("name", "_csrf");
+	const header = getMetaContent("name", "_csrf_header");
+	xhr.setRequestHeader(header, token);
 	xhr.send();
-}
+};
+
+const getMetaContent = (property, name) => document.head.querySelector("[" + property + "=" + name + "]").content;
