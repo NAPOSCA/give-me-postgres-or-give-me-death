d6771fa26f265fb73741461f9d6aa0b72e677dd4
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-08-01 00:39:39 -0400

    Pressing the delete category button hopefully deletes the category

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,5 +1,23 @@
 function initialize() {
+	const categoryId = document.querySelector("#id");
 	const detailsForm = document.querySelector(".category-details");
 	const updateButton = detailsForm.querySelector(".update");
 	const deleteButton = detailsForm.querySelector(".delete");
+	deleteButton.addEventListener("click", () => {
+		request(response => {
+			document.location.reload(true);
+		}, "DELETE", `/admin/category/${categoryId}`);
+	});
+}
+
+function request(callback, method, path) {
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			const response = JSON.parse(xhr.response);
+			callback(response);
+		}
+	};
+	xhr.open(method, path, true);
+	xhr.send();
 }
