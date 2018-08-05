590d89536b8f958f57f888b1e160585166c7eaf2
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-08-05 19:13:24 -0400

    Added visibility toggling for categories and their products

--- src/main/resources/static/js/shopping.js
+++ src/main/resources/static/js/shopping.js
@@ -1,4 +1,17 @@
 function initialize() {
+    const manyCategoryItemsDivs = document.querySelectorAll(".category-items");
+	for (let i = 0; i < manyCategoryItemsDivs.length; i++) {
+		const categoryDiv = manyCategoryItemsDivs[i];
+		const categorySection = categoryDiv.querySelector(".category");
+		const indicators = categorySection.querySelectorAll(".indicator");
+		const items = categoryDiv.querySelector(".items");
+		categorySection.addEventListener("click", () => {
+			toggleVisibility(items);
+			indicators.forEach(indicator => {
+				toggleClasses(indicator, "collapsed", "expanded");
+			});
+		});
+	}
 }
 
 function toggleClasses(element) {
