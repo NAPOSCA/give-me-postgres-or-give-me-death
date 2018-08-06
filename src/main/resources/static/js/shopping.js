6e5d525eefa28cb732749492711d823d2f183ab3
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-08-06 16:13:12 -0400

    On clicking plus button that product is added to the cart

--- src/main/resources/static/js/shopping.js
+++ src/main/resources/static/js/shopping.js
@@ -15,7 +15,12 @@ function initialize() {
 	const dichotomousProductButtons = document.querySelectorAll(".icon.dichotomous-product");
 	for (let i = 0; i < dichotomousProductButtons.length; i++) {
 		const dichotomousProductButton = dichotomousProductButtons[i];
+		const productId = dichotomousProductButton.parentElement.parentElement.value;
 		dichotomousProductButton.addEventListener("click", () => {
+			request(response => {
+				console.log("Added product");
+				console.log(JSON.parse(response));
+			}, "POST", `/cart/products/${productId}`);
 			toggleClasses(dichotomousProductButton, "plus", "x");
 		});
 	}
@@ -30,3 +35,19 @@ function toggleClasses(element) {
 function toggleVisibility(element) {
     toggleClasses(element, "hidden", "visible");
 }
+
+const request = (callback, method, url) => {
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			callback(xhr.response);
+		}
+	};
+	xhr.open(method, url, true);
+	const token = getMetaContent("name", "_csrf");
+	const header = getMetaContent("name", "_csrf_header");
+	xhr.setRequestHeader(header, token);
+	xhr.send();
+};
+
+const getMetaContent = (property, name) => document.head.querySelector("[" + property + "=" + name + "]").content;
