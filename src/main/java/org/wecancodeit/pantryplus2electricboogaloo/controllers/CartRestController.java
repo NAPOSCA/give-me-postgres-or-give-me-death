a88c47525f3c7c853ec161b15d3e289b219a52b3
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-08-07 01:55:25 -0400

    Added appropiate post mapping

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.wecancodeit.pantryplus2electricboogaloo.LoginService;
 import org.wecancodeit.pantryplus2electricboogaloo.cart.Cart;
 import org.wecancodeit.pantryplus2electricboogaloo.lineitem.LineItem;
@@ -23,6 +24,7 @@ public class CartRestController {
 	@Resource
 	private LineItemRepository lineItemRepo;
 	
+	@PostMapping("/cart/products/1")
 	public void receivePostOnProduct(@AuthenticationPrincipal OAuth2User googleId, long productId) {
 		PantryUser user = loginService.resolveUser(googleId);
 		Cart cart = user.getCart();
