401bd128a45a45dbd9a0ff704af7a3e4cb74a6df
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-08-07 02:37:08 -0400

    Swapped @Controller for @RestController

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
@@ -4,9 +4,9 @@ import javax.annotation.Resource;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.wecancodeit.pantryplus2electricboogaloo.LoginService;
 import org.wecancodeit.pantryplus2electricboogaloo.cart.Cart;
 import org.wecancodeit.pantryplus2electricboogaloo.lineitem.LineItem;
@@ -15,7 +15,7 @@ import org.wecancodeit.pantryplus2electricboogaloo.product.Product;
 import org.wecancodeit.pantryplus2electricboogaloo.product.ProductRepository;
 import org.wecancodeit.pantryplus2electricboogaloo.user.PantryUser;
 
-@Controller
+@RestController
 public class CartRestController {
 
 	@Resource
@@ -28,13 +28,12 @@ public class CartRestController {
 	private LineItemRepository lineItemRepo;
 
 	@PostMapping("/cart/products/{productId}")
-	public String receivePostOnProduct(@AuthenticationPrincipal OAuth2User googleId, @PathVariable long productId) {
+	public void receivePostOnProduct(@AuthenticationPrincipal OAuth2User googleId, @PathVariable long productId) {
 		PantryUser user = loginService.resolveUser(googleId);
 		Cart cart = user.getCart();
 		Product product = productRepo.findById(productId).get();
 		LineItem lineItem = new LineItem(cart, product);
 		lineItemRepo.save(lineItem);
-		return "success";
 	}
 
 }
