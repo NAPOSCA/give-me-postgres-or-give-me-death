855d1d9a10f02ae8633dc1f2b23a4cee427f982f
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-08-08 18:32:45 -0400

    If PricedProduct, rest api adds its updated currency used by cart

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/CartRestController.java
@@ -1,8 +1,12 @@
 package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -14,10 +18,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.wecancodeit.pantryplus2electricboogaloo.LoginService;
 import org.wecancodeit.pantryplus2electricboogaloo.cart.Cart;
+import org.wecancodeit.pantryplus2electricboogaloo.cart.CartRepository;
+import org.wecancodeit.pantryplus2electricboogaloo.currency.Currency;
+import org.wecancodeit.pantryplus2electricboogaloo.currency.CurrencyRepository;
 import org.wecancodeit.pantryplus2electricboogaloo.lineitem.CountedLineItem;
 import org.wecancodeit.pantryplus2electricboogaloo.lineitem.LineItem;
 import org.wecancodeit.pantryplus2electricboogaloo.lineitem.LineItemRepository;
 import org.wecancodeit.pantryplus2electricboogaloo.product.LimitedProduct;
+import org.wecancodeit.pantryplus2electricboogaloo.product.PricedProduct;
 import org.wecancodeit.pantryplus2electricboogaloo.product.Product;
 import org.wecancodeit.pantryplus2electricboogaloo.product.ProductRepository;
 import org.wecancodeit.pantryplus2electricboogaloo.user.PantryUser;
@@ -33,6 +41,15 @@ public class CartRestController {
 
 	@Resource
 	private LineItemRepository lineItemRepo;
+
+	@Resource
+	private EntityManager entityManager;
+
+	@Resource
+	private CartRepository cartRepo;
+
+	@Resource
+	private CurrencyRepository currencyRepo;
 
 	@PostMapping("/cart/products/{productId}")
 	public void receivePostOnProduct(@AuthenticationPrincipal OAuth2User googleId, @PathVariable long productId) {
@@ -55,24 +72,40 @@ public class CartRestController {
 		}
 	}
 
+	@Transactional
 	@PutMapping("/cart/products/{productId}")
-	public int receivePutOnProduct(@AuthenticationPrincipal OAuth2User googleId, @PathVariable long productId,
-			@RequestParam int quantity) {
+	public Map<String, Object> receivePutOnProduct(@AuthenticationPrincipal OAuth2User googleId,
+			@PathVariable long productId, @RequestParam int quantity) {
 		Cart cart = loginService.resolveUser(googleId).getCart();
 		LimitedProduct product = (LimitedProduct) productRepo.findById(productId).get();
 		CountedLineItem lineItem = cart.getCountedLineItemContaining(productId)
 				.orElseGet(() -> new CountedLineItem(cart, product));
+		Map<String, Object> json = new HashMap<>();
 		if (quantity <= 0) {
 			lineItemRepo.delete(lineItem);
-			return 0;
+			json.put("quantity", 0);
+		} else {
+			int maxQuantity = product.getMaximumQuantity();
+			if (quantity > maxQuantity) {
+				quantity = maxQuantity;
+			}
+			lineItem.setQuantity(quantity);
+			lineItem = lineItemRepo.save(lineItem);
+			json.put("quantity", lineItem.getQuantity());
 		}
-		int maxQuantity = product.getMaximumQuantity();
-		if (quantity > maxQuantity) {
-			quantity = maxQuantity;
+		if (product.getType().equals("PricedProduct")) {
+			PricedProduct pricedProduct = (PricedProduct) product;
+			long currencyId = pricedProduct.getCurrency().getId();
+			json.put("currencyId", currencyId);
+			long cartId = cart.getId();
+			entityManager.flush();
+			entityManager.clear();
+			Cart cartAgain = cartRepo.findById(cartId).get();
+			Currency currency = currencyRepo.findById(currencyId).get();
+			int amountUsed = cartAgain.amountUsed(currency);
+			json.put("amountUsed", amountUsed);
 		}
-		lineItem.setQuantity(quantity);
-		lineItem = lineItemRepo.save(lineItem);
-		return lineItem.getQuantity();
+		return json;
 	}
 
 }
