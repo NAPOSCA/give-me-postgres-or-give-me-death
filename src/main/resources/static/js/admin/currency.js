0108c40ed25f6a0777ae55a097ce4dc96cdab15a
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-09-12 15:58:30 -0400

    Encoded the parameters of the URI

--- src/main/resources/static/js/admin/currency.js
+++ src/main/resources/static/js/admin/currency.js
@@ -3,9 +3,9 @@ function initialize() {
 	const detailsForm = document.querySelector(".currency-details");
 	const updateButton = document.querySelector("#update");
 	updateButton.addEventListener("click", () => {
-		const name = detailsForm.querySelector("input.name").value;
-		const unit = detailsForm.querySelector("input.unit").value;
-		const allowanceMap = detailsForm.querySelector("input.allowanceMap").value;
+		const name = encodeURIComponent(detailsForm.querySelector("input.name").value);
+		const unit = encodeURIComponent(detailsForm.querySelector("input.unit").value);
+		const allowanceMap = encodeURIComponent(detailsForm.querySelector("input.allowanceMap").value);
 		request(response => {
 			reload();
 		}, "PUT", `/admin/currency/${currencyId}?name=${name}&unit=${unit}&allowanceMap=${allowanceMap}`);
