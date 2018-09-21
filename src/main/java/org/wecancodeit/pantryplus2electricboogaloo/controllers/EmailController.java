4fa61f404bacb191faccd44ed6a36d3cd78ebeb3
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-09-21 17:59:59 -0400

    Forgot to add the annotation to resolve a user's principal

--- src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/EmailController.java
+++ src/main/java/org/wecancodeit/pantryplus2electricboogaloo/controllers/EmailController.java
@@ -3,6 +3,7 @@ package org.wecancodeit.pantryplus2electricboogaloo.controllers;
 import javax.annotation.Resource;
 import javax.mail.MessagingException;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,7 +26,7 @@ public class EmailController {
 	private HtmlMailSender sender;
 
 	@RequestMapping("/email")
-	public String sendEmail(OAuth2User googleId, Model model) throws MessagingException {
+	public String sendEmail(@AuthenticationPrincipal OAuth2User googleId, Model model) throws MessagingException {
 		PantryUser user = loginService.resolveUser(googleId);
 		model.addAttribute("user", user);
 		model.addAttribute("cart", user.getCart());
